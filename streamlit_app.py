1c94968f0aed81d2fa862a404f593ec6580f37c1
nnadasalsabila <113287817+nnadasalsabila@users.noreply.github.com>
2025-09-09 22:28:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -378,7 +378,7 @@ elif menu == "📊 Pemodelan & Prediksi":
   
           if 'data' in locals() and data is not None:
               data.index = pd.to_datetime(data.index)
-              if category == "Keriting"
+              if category == "Keriting":
                  split_date = '2024-12-26'
               else:
                  split_date = '2024-12-25'
